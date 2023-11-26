e33ff4a18081367bc12944f2c14609a6432d576a
mlchanjc <chanmanlok0808@gmail.com>
2023-11-27 00:18:45 +0800

    prototype of phone shake mission

--- app/src/main/java/com/example/alarmapp/MainActivity.kt
+++ app/src/main/java/com/example/alarmapp/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.alarmapp.components.games.MemoryGame
+import com.example.alarmapp.components.missions.PhoneShaking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             //MemoryGame()
-            AlarmClockApp()
+            //AlarmClockApp()
+            PhoneShaking()
         }
     }
 }
